9ac9bbf05afffefff311bffd7465be47f00e1052
Abhishek <abhishek@hutechsolutions.com>
2022-08-08 16:37:48 +0530

    file added

---
+++ src/test/java/com/hutech/userexpenseinfo/UserExpenseInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hutech.userexpenseinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserExpenseInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
